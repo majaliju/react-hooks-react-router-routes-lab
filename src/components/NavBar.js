f91becd973c5c6e77e80a3bef30feda44aeafbad
majaliju <majalijunyc@gmail.com>
2022-06-21 21:06:36 -0400

    updated Home, App, and NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,25 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { NavLink } from "react-router-dom";
 
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <ul>
+      <li><NavLink to="/movies" exact>
+        Movies 
+      </NavLink></li>
+      <li><NavLink to="/directors" exact>
+        Directors 
+      </NavLink></li>
+      <li><NavLink to="/actors" exact>
+        Actors 
+      </NavLink></li>
+      <li><NavLink to="/" exact>
+        Home 
+      </NavLink></li>
+    </ul>
+  );
 }
 
 export default NavBar;
